4d98094a452371955604f5808f258f5386dee73d
DarkAeterna <artur-mergasov@mail.ru>
2026-01-18 19:55:50 +0300

    refactoring: PR refactor

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -1,30 +1,26 @@
+
 using UnityEngine;
 
-
+[RequireComponent(typeof(Health))]
 [RequireComponent(typeof(Collider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class Base : MonoBehaviour
 {
-    [SerializeField] private float _baseHp = 100.0f;
-   
-    private void OnCollisionEnter2D(Collision2D collision)
+    private Health _health;
+    
+    private void Awake()
     {
-        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+        _health = GetComponent<Health>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Enemy enemy = other.GetComponent<Enemy>();
+
         if (enemy != null)
         {
-            TakeDamage(10f);
+            _health.TryTakeDamage(10f);
             Destroy(enemy.gameObject);
-            Debug.Log($"Enemy destroyed! Base HP: {_baseHp}");
-        }
-    }
-   
-    private void TakeDamage(float damage)
-    {
-        _baseHp -= damage;
-        if (_baseHp <= 0)
-        {
-            Debug.Log("Base destroyed!");
-            Destroy(gameObject);
         }
     }
 }
